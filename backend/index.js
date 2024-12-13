150f24a5ede37e219bd674ecbe9a5807bc1708b8
ritikaaggarwall <ritikaaggarwal1222@gmail.com>
2024-12-13 14:17:33 +0530

    Added my project

--- backend/index.js
+++ backend/index.js
@@ -21,19 +21,15 @@ const corsOptions = {
     credentials:true
 }
 
-
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
-
 
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
-
-
 
 app.listen(PORT,()=>{
     connectDB();
